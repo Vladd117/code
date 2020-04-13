0a7dee42582cd0904448ce9c9808b60d74191b2c
Vladislav <vladd117@gmail.com>
2020-04-13 20:20:27 +0300

    DOM learning

--- dom/js/script.js
+++ dom/js/script.js
@@ -1,22 +1,44 @@
-button1 = {
+let button1 = {
     beckColor: 'black',
     textColor: 'burlywood'
 };
-newColor = function(color) {
+let newColor = function(color) {
     let element = document.getElementById("btn01");
     element.style.backgroundColor = color;
 };
-nColor = function(color) {
+let nColor = function(color) {
     let element = document.getElementById("btn02");
     element.style.backgroundColor = color;
     element.innerHTML = color;
 };
 
 function validateForm() {
-    let element = document.forms["form01"]["fname"].value;
+    let element = document.forms.form01.fname.value;
     if (element == "") {
         alert("Name is empty");
         return false;
     }
 
 }
+let red = 1;
+let redText = 254;
+let i = 1;
+
+let box = document.querySelectorAll('.box');
+console.log(box);
+let color = window.setInterval(() => {
+    red += i;
+    redText -= i;
+    box[0].style.backgroundColor = 'rgb(' + red + ',0,0)';
+    box[0].style.color = 'rgb(' + redText + ',0,0)';
+    if (red > 254 || red < 1) {
+        console.log(i);
+        i *= -1;
+
+        //red = 0;
+    }
+}, 30);
+console.log(i);
+
+
+//console.log(red);
