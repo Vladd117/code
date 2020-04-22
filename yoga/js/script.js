84d5b3ae40f0ca41ab80281242362d101f0214cb
Vladislav <vladd117@gmail.com>
2020-04-22 20:10:50 +0300

    Add Timer in Yoga

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded',
     function() {
+
+        // Tabs ------------------------------------->
+
         'use strict';
         let tab = document.querySelectorAll('.info-header-tab'),
             info = document.querySelector('.info-header'),
@@ -31,4 +34,49 @@ window.addEventListener('DOMContentLoaded',
                 }
             }
         });
+
+        // Таймер! ---------------------------------->
+
+        let deadline = '2020-04-23';
+
+        function getTimeRemainig(endTime) {
+            let t = Date.parse(deadline) - Date.parse(new Date()) - 3 * 60 * 60 * 1000,
+                seconds = Math.floor((t / 1000) % 60),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                hours = Math.floor((t / 1000 / 60 / 60) % 24),
+                deys = Math.floor(t / 1000 / 60 / 60 / 24);
+            return {
+                total: t,
+                seconds: seconds,
+                minutes: minutes,
+                hours: hours,
+                deys: deys,
+            };
+        }
+
+        function setClock(id, endTime) {
+            let timer = document.getElementById(id),
+                deys = timer.querySelector('.deys'),
+                hours = timer.querySelector('.hours'),
+                minutes = timer.querySelector('.minutes'),
+                seconds = timer.querySelector('.seconds'),
+                timeInt = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = getTimeRemainig(endTime);
+                deys.textContent = t.deys;
+                hours.textContent = t.hours;
+                if (t.minutes < 10) {
+                    minutes.textContent = "0" + t.minutes;
+                } else { minutes.textContent = t.minutes; }
+                if (t.seconds < 10) {
+                    seconds.textContent = "0" + t.seconds;
+                } else { seconds.textContent = t.seconds; }
+                if (t.total <= 0) {
+                    clearInterval(timeInt);
+                }
+            }
+
+        }
+        setClock('timer', deadline);
     });
