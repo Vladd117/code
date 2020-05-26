253c284a79122152c0507af2bd8a7ba129bcfa07
Vladislav <vladd117@gmail.com>
2020-05-26 08:40:01 +0300

    misc

--- Test001/js/script.js
+++ Test001/js/script.js
@@ -22,10 +22,22 @@ let person = {
     }
 };
 
-
 hello();
 person.sayHello();
 person.sayHalloWin();
 person.sayHelloPerson2();
 person.say();
 person.logInfo.call(person2);
+
+let disp = document.querySelector('.bg');
+
+let foo = function(n) {
+    let c = n * 9;
+    let d = 0;
+    for (let i = 0; i < c; i++) {
+        d += c;
+    }
+    return d;
+};
+console.log(foo.prototype);
+disp.innerHTML = (`Result of function ${foo(5)}, function prototype: ${foo.prototype}`);
