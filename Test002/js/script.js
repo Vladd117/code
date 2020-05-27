25a13976e976e18a7068ce8f57b69460c9df8cd4
Vladislav <vladd117@gmail.com>
2020-05-27 08:30:12 +0300

    scope

---
+++ Test002/js/script.js
@@ -0,0 +1,22 @@
+let person1 = {
+    name: 'vasya',
+    age: 28,
+    job: 'frontend'
+};
+let person2 = {
+    name: 'masha',
+    age: 24,
+    job: 'SMM'
+};
+
+function personLog(person) {
+    let logOut = function() {
+        console.log(`Persons name: ${this.name}, age is ${this.age}, job is ${this.job}`);
+    };
+    return logOut.bind(person);
+}
+let pers1 = personLog(person1);
+let pers2 = personLog(person2);
+
+pers1();
+pers2();
